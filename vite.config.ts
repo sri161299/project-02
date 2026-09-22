6281b4e528a9607f2669080d0c530cf6848a1837
sri161299 <srinandhini1612@gmail.com>
2026-09-22 14:24:21 +0530

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/project-02/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
